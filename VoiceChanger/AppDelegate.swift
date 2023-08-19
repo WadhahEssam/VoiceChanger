aa5943228771115f83f4e6a1116ae99fcdb527b1
Wadah Esam <wadah-wleed@hotmail.com>
2023-08-19 19:03:53 +0300

    .

--- VoiceChanger/AppDelegate.swift
+++ VoiceChanger/AppDelegate.swift
@@ -7,12 +7,10 @@
 
 import UIKit
 
-@main
+@main // indicates that this is the entry point of the application
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        print("app is launched")
         // Override point for customization after application launch.
         return true
     }
@@ -20,17 +18,17 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     // MARK: UISceneSession Lifecycle
 
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        print("creating new scene session")
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
 
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+        print("discarding session")
         // Called when the user discards a scene session.
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
-
-
 }
 
